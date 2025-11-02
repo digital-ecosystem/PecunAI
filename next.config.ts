e2a5d0b6aa1dab10314481b32ed29a2c9f45ce6b
Bassem Mahdi <bassembinmahdi@gmail.com>
2025-11-02 16:37:56 +0100

    update the cross and the port to use 4001

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  allowedDevOrigins: [
+    'http://localhost:4001',
+    'https://ai.digital-ecosystem.management'
+  ],
 };
 
 export default nextConfig;
