4e9cc17ee915b282522031f18c23de4aefecca30
Bassem Mahdi <bassembinmahdi@gmail.com>
2025-11-02 16:40:43 +0100

    update the cross

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  allowedDevOrigins: [
-    'http://localhost:4001',
-    'https://ai.digital-ecosystem.management'
-  ],
+  allowedDevOrigins: ['https://ai.digital-ecosystem.management'],
 };
 
 export default nextConfig;
